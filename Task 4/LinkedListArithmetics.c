f907ac0c79b013a72b5df5272d7170fda56116f2
Callum Gran <callum.gran@gmail.com>
2022-09-14 16:44:51 +0200

    finished task 1 on exercise 4

--- Task 4/LinkedListArithmetics.c
+++ Task 4/LinkedListArithmetics.c
@@ -73,22 +73,6 @@ Node* find_nr(DoubleLink *l, int nr)
     else return NULL;
 }
 
-void delete_all(DoubleLink *l)
-{
-    if(l->head)
-    {
-        Node* this = l->head;
-        do {
-            l->head = this->next;
-            free(this);
-            this = l->head;
-        } while(this->next);
-        free(this);
-        l->head = NULL;
-        l->size = 0;
-    }
-}
-
 void start(Iterator *iter, DoubleLink* l)
 {
     iter->place = l->head;
@@ -125,52 +109,81 @@ void addition(DoubleLink *l, Node *a, int b)
     }
 }
 
-void addition_list(DoubleLink *l1, DoubleLink *l2)
-{
-    int i;
-    int place = l1->size - l2->size;
-    Iterator *iter_a = malloc(sizeof(Iterator))
-    , *iter_b = malloc(sizeof(Iterator));
-    start(iter_a, l1);
-    start(iter_b, l2);
-    for (i = 0; i < place; i++)
-    {
-        next(iter_a);
-    }
-
-    while(!(end(iter_a) || end(iter_b))) 
-    {
-        addition(l1, iter_a->place, iter_b->place->element);
-        next(iter_a);
-        next(iter_b);
-    }
-    free(iter_a);
-    free(iter_b);
-}
-
 void subtraction(DoubleLink *l, Node *a, int b) 
 {
     a->element = a->element - b;
-    if (a->element < 1)
+    if (a->element < 0)
     {
-        if (a->prev == NULL && a->prev == l->head) 
+        a->element = a->element + 10;
+        subtraction(l, a->prev, 1);
+        if (l->head->element == 0)
         {
             remove_node(l, l->head);
-        } else {
-            addition(l, a, 10);
-            a->element = a->prev->element - 1;
+            l->head = a;
         }
     }
 }
 
-void subtraction_list(DoubleLink *l1, DoubleLink *l2)
+void swap_lists(DoubleLink **l1, DoubleLink **l2)
 {
-    int i;
-    int place = l1->size - l2->size;
+    DoubleLink *temp = malloc(sizeof(DoubleLink));
     Iterator *iter_a = malloc(sizeof(Iterator))
     , *iter_b = malloc(sizeof(Iterator));
+
+    int size_l1 = (*l1)->size + 1
+    , size_l2 = (*l2)->size + 1
+    , i;
+
+    start(iter_a, *l2);
+    start(iter_b, *l1);
+
+    while(!end(iter_a))
+    {
+        add_last_pos(temp, iter_a->place->element);
+        next(iter_a);
+    }
+
+    while (!end(iter_b))
+    {
+        add_last_pos(*l2, iter_b->place->element);
+        next(iter_b);
+    }
+
+    start(iter_a, temp);
+    while(!end(iter_a))
+    {
+        add_last_pos(*l1, iter_a->place->element);
+        next(iter_a);
+    }
+    
+    for (i = 0; i < size_l2; i++) 
+    {
+        remove_node(*l2, (*l2)->head);
+    }
+    for (i = 0; i < size_l1; i++) 
+    {
+        remove_node(*l1, (*l1)->head);
+    }
+    free(iter_a);
+    free(iter_b);
+    free(temp);
+}
+
+void arithmetic(DoubleLink *l1, DoubleLink *l2, void (func)(DoubleLink*, Node*, int))
+{
+    int i, place;
+    Iterator *iter_a = malloc(sizeof(Iterator))
+    , *iter_b = malloc(sizeof(Iterator));
+
+    if (func == addition && l2->size > l1->size) 
+    {
+        swap_lists(&l1, &l2);
+    }
+
     start(iter_a, l1);
     start(iter_b, l2);
+    place = l1->size - l2->size;
+
     for (i = 0; i < place; i++)
     {
         next(iter_a);
@@ -178,18 +191,19 @@ void subtraction_list(DoubleLink *l1, DoubleLink *l2)
 
     while(!(end(iter_a) || end(iter_b))) 
     {
-        subtraction(l1, iter_a->place, iter_b->place->element);
+        func(l1, iter_a->place, iter_b->place->element);
         next(iter_a);
         next(iter_b);
     }
+
     free(iter_a);
     free(iter_b);
 }
 
-void printDoubleLink(DoubleLink *l1, DoubleLink *l2,  void (func)(DoubleLink*, DoubleLink*))
+void printDoubleLink(DoubleLink *l1, DoubleLink *l2, void (func)(DoubleLink*, Node*, int))
 {
     printf("Here is the answer\n");
-    func(l1, l2);
+    arithmetic(l1, l2, func);
     Node *cur = l1->head;
     while (cur != NULL) 
     {
@@ -206,10 +220,22 @@ int main(int argc, char *argv[])
      * argv[2] = operator (+ or -)
      * argv[3] = second number
      */
+    void (*func)(DoubleLink*, Node*, int);
     if (argc != 4) 
     {
         fprintf(stderr, "wrong amount of input\n");
         return 1;
+    }
+    if (*argv[2] != '-' && *argv[2] != '+') 
+    {
+        fprintf(stderr, "This program only allows addition and subtraction.\n");
+        return 1;
+    } else if (*argv[2] == '-')
+    {
+        func = &subtraction;
+    } else
+    {
+        func = &addition;
     }
     char c, c1, c2;
     c1 = argv[1][0];
@@ -229,7 +255,7 @@ int main(int argc, char *argv[])
     while ((c = *(++argv[3])) != 0)
         add_last_pos(b, (int)c - '0');
     
-    printDoubleLink(a, b, &subtraction_list);
+    printDoubleLink(a, b, func);
     free(a);
     free(b);
 }
